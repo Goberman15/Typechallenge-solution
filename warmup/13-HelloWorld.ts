e001750d9e13ad5b57fbbcee53658efd49f80cb4
goberman15 <akbarrmdhn94@gmail.com>
2022-01-10 19:11:00 +0700

    solve 13-Hello World

---
+++ warmup/13-HelloWorld.ts
@@ -0,0 +1,23 @@
+/*
+	13 - Hello World
+	-------
+	by Anthony Fu (@antfu) #warm-up 
+*/
+
+//  Question
+
+/**
+	Hello, World!
+
+	In Type Challenges, we use the type system itself to do the assertion. 
+
+
+	For this challenge, you will need to change the following code to make the tests pass (no type check errors).
+	
+	// expected to be string
+	type HelloWorld = any
+*/
+
+/* __________________ Answer __________________ */
+
+type HelloWorld = string // expected to be a string

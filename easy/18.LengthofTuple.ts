bcb4459f09993d619f1095399cb8f5f588bab913
goberman15 <akbarrmdhn94@gmail.com>
2022-01-12 14:09:28 +0700

    solve 18.Length of Tuple

---
+++ easy/18.LengthofTuple.ts
@@ -0,0 +1,14 @@
+/*
+  18 - Length of Tuple
+  -------
+  by sinoon (@sinoon) #easy #tuple
+  
+  ### Question
+  
+  For given a tuple, you need create a generic `Length`, pick the length of the tuple
+*/
+
+
+/* __________________ Answer __________________ */
+
+type Length<T extends readonly any[]> = T[keyof { length: number }]

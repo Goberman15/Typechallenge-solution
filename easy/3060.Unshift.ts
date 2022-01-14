99a185b3686764f325d6b6f1965f146d11f6d8bf
goberman15 <akbarrmdhn94@gmail.com>
2022-01-14 15:42:11 +0700

    solve 3060-Unshift

---
+++ easy/3060.Unshift.ts
@@ -0,0 +1,21 @@
+/*
+  3060 - Unshift
+  -------
+  by jiangshan (@jiangshanmeta) #easy #array
+  
+  ### Question
+  
+  Implement the type version of ```Array.unshift```
+  
+  For example
+  
+  ```typescript
+  type Result = Unshift<[1, 2], 0> // [0, 1, 2,]
+  ```
+  
+  > View on GitHub: https://tsch.js.org/3060
+*/
+
+/* __________________ Answer __________________ */
+
+type Unshift<T extends any[], U> = [U, ...T]

c6138b9b8b73ed45bf3f03813deba72dfaa39d3e
goberman15 <akbarrmdhn94@gmail.com>
2022-01-15 07:07:15 +0700

    solve 3-Omit

---
+++ medium/3.Omit.ts
@@ -0,0 +1,33 @@
+/*
+  3 - Omit
+  -------
+  by Anthony Fu (@antfu) #medium #union #built-in
+  
+  ### Question
+  
+  Implement the built-in `Omit<T, K>` generic without using it.
+  
+  Constructs a type by picking all properties from `T` and then removing `K`
+  
+  For example
+  
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+  
+  type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+  
+  const todo: TodoPreview = {
+    completed: false,
+  }
+  ```
+*/
+
+/* __________________ Answer __________________ */
+
+type MyOmit<T, K> = {
+    [Prop in keyof T as Prop extends K ? never : Prop]: T[Prop]
+}
